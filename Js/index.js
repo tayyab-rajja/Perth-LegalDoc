23b8a16dd4402c05292ca099efc7d6c3238d3598
Tayyab Raja <tayyabraja316@gmail.com>
2022-10-18 04:54:54 +0500

    fix landing page issues, toggle button mobile view  and slider mobile view left

--- Js/index.js
+++ Js/index.js
@@ -1,104 +1,33 @@
-jQuery('.card-slider').slick({
-    slidesToShow:3,
-    autoplay: true,
-    slidesToScroll:1,
-    dots: false,
-    responsive:[
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-  
-  let cards = document.querySelectorAll('.card');
-  let card;
-  let modal = document.querySelector('.modal');
-  let closeButton = document.querySelector('.modal__close-button');
-  let page = document.querySelector('.page');
-  const cardBorderRadius = 20;
-  const openingDuration = 600; //ms
-  const closingDuration = 600; //ms
-  const timingFunction = 'cubic-bezier(.76,.01,.65,1.38)';
-
-  
-  var Scrollbar = window.Scrollbar;
-  Scrollbar.init(document.querySelector('.modal__scroll-area'));
-  
-  
-  function flipAnimation(first, last, options) {
-    let firstRect = first.getBoundingClientRect();
-    let lastRect = last.getBoundingClientRect();
-    
-    let deltas = {
-      top: firstRect.top - lastRect.top,
-      left: firstRect.left - lastRect.left,
-      width: firstRect.width / lastRect.width,
-      height: firstRect.height / lastRect.height
-    };
-    
-    return last.animate([{
-      transformOrigin: 'top left',
-      borderRadius:`
-      ${cardBorderRadius/deltas.width}px / ${cardBorderRadius/deltas.height}px`,
-      transform: `
-        translate(${deltas.left}px, ${deltas.top}px) 
-        scale(${deltas.width}, ${deltas.height})
-      `
-    },{
-      transformOrigin: 'top left',
-      transform: 'none',
-      borderRadius: `${cardBorderRadius}px`
-    }],options);
-  }
-  
-  cards.forEach((item)=>{
-    item.addEventListener('click',(e)=>{
-      jQuery('.card-slider').slick('slickPause');
-      card = e.currentTarget;
-      page.dataset.modalState = 'opening';
-      card.classList.add('card--opened');
-      card.style.opacity = 0;
-      document.querySelector('body').classList.add('no-scroll');
-  
-      let animation = flipAnimation(card,modal,{
-        duration: openingDuration,
-        easing: timingFunction,
-        fill:'both'
+document.addEventListener("DOMContentLoaded", function () {
+  // make it as accordion for smaller screens
+  if (window.innerWidth < 992) {
+    // close all inner dropdowns when parent is closed
+    document
+      .querySelectorAll(".navbar .dropdown")
+      .forEach(function (everydropdown) {
+        everydropdown.addEventListener("hidden.bs.dropdown", function () {
+          // after dropdown is hidden, then find all submenus
+          this.querySelectorAll(".submenu").forEach(function (everysubmenu) {
+            // hide every submenu as well
+            everysubmenu.style.display = "none";
+          });
+        });
       });
-  
-      animation.onfinish = ()=>{
-        page.dataset.modalState = 'open';
-        animation.cancel();
-      };
-    });
-  });
-  
-  closeButton.addEventListener('click',(e)=>{
-    page.dataset.modalState = 'closing';
-    document.querySelector('body').classList.remove('no-scroll');
-    
-    let animation = flipAnimation(card,modal,{
-      duration: closingDuration,
-      easing: timingFunction,
-      direction: 'reverse',
-      fill:'both'
-    });
-  
-    animation.onfinish = ()=>{
-      page.dataset.modalState = 'closed';
-      card.style.opacity = 1;
-      card.classList.remove('card--opened');
-      jQuery('.card-slider').slick('slickPlay');
-      animation.cancel();
-    };
-  });
 
+    document.querySelectorAll(".dropdown-menu a").forEach(function (element) {
+      element.addEventListener("click", function (e) {
+        let nextEl = this.nextElementSibling;
+        if (nextEl && nextEl.classList.contains("submenu")) {
+          // prevent opening link if link needs to open dropdown
+          e.preventDefault();
+          if (nextEl.style.display == "block") {
+            nextEl.style.display = "none";
+          } else {
+            nextEl.style.display = "block";
+          }
+        }
+      });
+    });
+  }
+  // end if innerWidth
+});
